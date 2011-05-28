488019692d38474078a6f417a9bb85196ac34c46
Greg Conant <greg.conant@gmail.com>
2011-05-28 14:25:29 -0400

    adding correct problem #s to Euler problems

---
+++ Euler-5.rb
@@ -0,0 +1,25 @@
+#Euler # 4
+#2520 is the smallest number that can be divided by each of the numbers from 
+#1 to 10 without any remainder.
+
+#What is the smallest positive number that is evenly divisible by all 
+#of the numbers from 1 to 20?
+
+
+def find_smallest_num_divisible_by_all(range)
+  qualifying = []
+  (1..9999999999).each do |curr|
+    success = true
+    range.each do |divisor|
+      if(curr % divisor != 0)
+        success = false
+        break
+      end
+    end
+    if success
+      return curr
+    end
+  end
+end
+
+puts find_smallest_num_divisible_by_all((1..20))
---
+++ Euler-6.rb
@@ -0,0 +1,34 @@
+#The sum of the squares of the first ten natural numbers is,
+
+#12 + 22 + ... + 102 = 385
+#The square of the sum of the first ten natural numbers is,
+
+#(1 + 2 + ... + 10)2 = 552 = 3025
+#Hence the difference between the sum of the squares of the 
+#first ten natural numbers and the square of the sum is 3025  385 = 2640.
+
+#Find the difference between the sum of the 
+#squares of the first one hundred natural numbers and the square of the sum.
+
+
+def sum_of_squares(num_count)
+  #print "sum of squares of 1 to ", num_count, "\n"
+  sum = 0
+  (1..num_count).each do |num|
+    sum += num**2
+  end
+  sum
+end
+
+def square_of_sums(num_count)
+  #print "square of sums of 1 to ", num_count, "\n"
+  sum = 0
+  (1..num_count).each do |num|
+    sum += num
+  end  
+  sum**2
+end
+
+result = square_of_sums(100) - sum_of_squares(100)
+print result
+# answer: 25164150 - GMC 20110516
